18050c0a7fc7b7ba32caf0f0fbbf9efe616227bd
LukeRuozzi <81956779+LukeRuozzi@users.noreply.github.com>
2023-07-22 12:56:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# theme-selector
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-ifvudp)
---
+++ script.js
@@ -0,0 +1,36 @@
+const radios = document.querySelectorAll("input[type='radio']");
+
+const defaultUiSettings = [
+  {
+    key: 'theme',
+    value: 'light',
+  },
+  {
+    key: 'border',
+    value: 'square',
+  },
+];
+
+window.addEventListener('DOMContentLoaded', () => {
+  defaultUiSettings.forEach((setting) => {
+    const value = localStorage.getItem(setting.key) ?? setting.value;
+    updateSiteUi(setting.key, value);
+    document.querySelector(`#${value}`).checked = true;
+  });
+});
+
+radios.forEach((radio) => {
+  radio.addEventListener('change', (e) => {
+    const { id, name } = e.target;
+    saveUIPref(name, id);
+    updateSiteUi(name, id);
+  });
+});
+
+const saveUIPref = (name, value) => {
+  localStorage.setItem(name, value);
+};
+
+const updateSiteUi = (name, value) => {
+  document.documentElement.dataset[name] = value;
+};
